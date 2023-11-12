c51a13f26ba77e75a2543cdf8e9bd9a4113ebf63
Silver Lui <55328122+siuchingovo@users.noreply.github.com>
2023-11-12 22:09:58 +0800

    mat update & graphics update

--- Assets/Scripts/Picture_control.cs
+++ Assets/Scripts/Picture_control.cs
@@ -8,12 +8,12 @@ public class Picture_control : MonoBehaviour
     public int pictureNum;
     public bool[] quest;
     public facialManager faceManager;
-    public Material pic_mat;
+    // public Material pic_mat;
     private float lip_value;
-    private float current_value = -1.1f;
+
     void Start()
     {
-        pic_mat = GetComponent<MeshRenderer>().material;
+        // pic_mat = GetComponent<MeshRenderer>().material;
     }
 
     
@@ -21,12 +21,13 @@ public class Picture_control : MonoBehaviour
     {
         if(quest[0] == true)
         {
-            if ((faceManager.lip_down_L + faceManager.lip_down_R)/2 > 0.0f) //Sad face -> Lip Corner Pull Down
+            lip_value = (faceManager.lip_down_L + faceManager.lip_down_R)/2; //Sad face -> Lip Corner Pull Down
+            if (lip_value > 0.0f) 
             {
-                lip_value = (faceManager.lip_down_L + faceManager.lip_down_R)/2;
                 
-                pic_value += lip_value*0.025f;
-                pic_mat.SetFloat("_cutOffHeight", pic_value);
+                
+                // pic_value += lip_value*0.025f;
+                // pic_mat.SetFloat("_cutOffHeight", pic_value);
                 
             }
         }
