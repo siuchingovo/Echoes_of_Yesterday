2145b687649c7c3da4004cae2ca54ebaf31ca1fd
chenyunwen <sssh10110@gmail.com>
2023-11-14 18:17:27 +0800

    update code for camera on picture

--- Assets/Scripts/FakeCameraCtrl.cs
+++ Assets/Scripts/FakeCameraCtrl.cs
@@ -5,35 +5,28 @@ using UnityEngine;
 public class FakeCameraCtrl : MonoBehaviour
 {
     public Transform playerCamera;
+    public Transform playerDefaultViewPoint;
     // public Transform Room2Camera;
-    private Vector3 Room1Pos;
-    private Quaternion Room1Rotate;
+    // private Quaternion Room1Rotate;
     public Transform Room2ViewPoint;
-    public Transform PictureViewPoint;
-    private Vector3 Room2Pos;
+    public Transform Picture;
+    private Vector3 player_to_photo;
+    private Quaternion Room1toRoom2Dir;
+    // private Vector3 Room2Pos;
     // Start is called before the first frame update
     void Start()
     {
-        // Room1Pos = playerCamera.position;
-        // Room1Pos = new Vector3(1.15f,0.8f,0.485f);
         // Room1Rotate = playerCamera.rotation;
-        // Room1Pos.rotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
-        // Room2Pos = new Vector3(0.145f,0.54f,8.371f);
-        // Room2Pos.rotation = Quaternion.Euler(new Vector3(0f,-58.30f,0f));
+        Room1toRoom2Dir = Quaternion.FromToRotation(Picture.position - playerDefaultViewPoint.position, Room2ViewPoint.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 player_offset = playerCamera.position - PictureViewPoint.position;
-        transform.position = Room2ViewPoint.position + player_offset;
+        Vector3 player_offset = playerCamera.position - playerDefaultViewPoint.position;
+        transform.position = Room2ViewPoint.position + Room1toRoom2Dir * player_offset;
 
-        float angularDiff = Quaternion.Angle(playerCamera.rotation, PictureViewPoint.rotation);
-        // float angularDiff = Quaternion.Angle(Quaternion.Euler(new Vector3(0f, 90f, 0f)), Quaternion.Euler(new Vector3(0f,-58.30f,0f)));
-
-        Quaternion RoomRotationDiff = Quaternion.AngleAxis(angularDiff, Vector3.up);
-        Vector3 newCameraDir = RoomRotationDiff * playerCamera.forward;
-        transform.rotation = Quaternion.LookRotation(newCameraDir, Vector3.up);
-        // Debug.Log(Quaternion.LookRotation(newCameraDir, Vector3.up));
+        // Vector3 player_view_rotate = playerCamera.forward;
+        transform.forward = Room1toRoom2Dir * playerCamera.forward;
     }
 }
