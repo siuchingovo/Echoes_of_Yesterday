de4335c0d85309f8293a087f638f493f9139b1ec
Pin910264 <128222863+Pin910264@users.noreply.github.com>
2023-12-22 23:32:08 +0800

    Update MRTrainController.cs

--- Assets/Scripts/MRTrainController.cs
+++ Assets/Scripts/MRTrainController.cs
@@ -10,6 +10,7 @@ public class MRTrainController : MonoBehaviour
     public EndOfPathInstruction endOfPathInstruction;
     public float distanceTravelled;
     public float speed;
+    public int currentPathPointIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +22,12 @@ public class MRTrainController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        while (currentPathPointIndex + 1 < pathCreator.path.localPoints.Length && pathCreator.path.GetClosestDistanceAlongPath(pathCreator.path.GetPoint(currentPathPointIndex)) <= distanceTravelled) {
+            currentPathPointIndex += 1;
+        }
         if (pathCreator != null && isRiding)
         {
+            speed = Vector3.Distance(pathCreator.path.GetPoint(currentPathPointIndex), pathCreator.path.GetPoint(currentPathPointIndex - 1)) / (60f/(54f * 3f / 4f * 3f));
             distanceTravelled += speed * Time.deltaTime;
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
             // transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction) * Quaternion.Euler(90, 0, 90);  // 我改回來了ㄏ 
