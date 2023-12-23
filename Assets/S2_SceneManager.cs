ff6115df1a797cd8d19d1cef46f1331085e39a85
chenyunwen <sssh10110@gmail.com>
2023-12-24 00:36:31 +0800

    S2 ending editing

---
+++ Assets/S2_SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class S2_SceneManager : MonoBehaviour
+{
+    [Header("Timeline")]
+    public PlayableDirector ending_timeline;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
